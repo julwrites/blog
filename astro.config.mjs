9c1d2ae10b28ad3d961e3ffa0d5a0ca56ad704c0
Julian <julian.teh@screeningeagle.com>
2023-02-05 00:46:38 +0800

    Attempting to use astro-spa to fix routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tehj.io/blog',
+  site: 'https://blog.tehj.io',
   integrations: [mdx(), sitemap(), vue()],
 });
