9459cf6057985bab79cb90210abf575a7ba11bf6
Julian <julian.teh@screeningeagle.com>
2023-02-04 22:15:31 +0800

    Updating with cloudflare pages integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,12 @@ import sitemap from '@astrojs/sitemap';
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), vue()]
+  integrations: [mdx(), sitemap(), vue()],
+  output: "server",
+  adapter: cloudflare()
 });
