055a5cdad0571c078ec76607477a749aca95ccaa
julwrites <mail@tehj.io>
2023-02-19 21:55:31 +0800

    Updating theme and adding a stub for new blog post

---
+++ 2023_02_19_Thoughts_On_Stewardship.md
@@ -0,0 +1,12 @@
+---
+title: "Thoughts on Stewardship"
+description: ""
+pubDate: "Feb 19 2023"
+draft: true
+---
+
+## Fatherhood is Stewardship
+
+## Stewardship is Responsibility
+
+
--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,5 @@
 export const SITE_TITLE = 'Julwrites Stuff';
 export const SITE_DESCRIPTION = 'Welcome to my log';
 export const SITE_BASEPATH = "/blog";
+export const SITE_COLOR_MODE = "dark";
+export const SITE_THEME = "prose dark:bg-neutral-800 dark:prose-invert prose-headings:text-primary-300 visited:prose-a:text-secondary-400";
